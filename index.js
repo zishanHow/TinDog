f06eb3288f05ba3285d04617d34b33dab3c62b73
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-11-01 23:44:33 +0600

    update feed dynamically with js.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("Hello World")
+const dogs = [
+    {
+        name: "Rex",
+        avatar: "images/dog-rex.jpg",
+        age: 25,
+        bio: "Art. Literature. Natural wine. Yoga.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Bella",
+        avatar: "images/dog-bella.jpg",
+        age: 43,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy",
+        avatar: "images/dog-teddy.jpg",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }
+]
+
+class Feed {
+    constructor(data){
+        Object.assign(this, data)
+    }
+
+    getFeedHtml(){
+        const {name, avatar, age, bio } = this
+        return `
+            <div class="feed-flex">
+                <img class="feed-img" src="${avatar}" alt="">
+
+                <div class="support-text">
+                    <p class="date">${name}, ${age}</p>
+                    <p class="caption">${bio}</p>
+                </div>
+            </div>
+        `
+    }
+}
+
+function render(){
+    document.getElementById("feed").innerHTML = tinDogs.getFeedHtml() 
+}
+
+let tinDogs = new Feed(dogs[1])
+
+render()
